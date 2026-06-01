fc598a46af9b41a177700d771283930b9f9729e0
TuaaanAnhNguyen <ntuananh2503@gmail.com>
2026-06-01 15:44:25 +0700

    merged with main for new backend codes

--- backend_dotnet/Models/Temp.cs
+++ backend_dotnet/Models/Temp.cs
@@ -3,6 +3,9 @@ using Supabase.Postgrest.Models;
 
 namespace BrokerViet.BackendDotnet.Models;
 
+// "Temp" table used solely to ping Supabase so it would not be paused due to inactivity;
+// DO NOT USE FOR ANY BUSINESS LOGIC PURPOSES
+
 [Table("temp")]
 public class Temp : BaseModel
 {
