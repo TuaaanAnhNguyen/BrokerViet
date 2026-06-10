152a185d291fd2813bc08f1ea0696217573b8f12
dakbink <146689097+dacbinh@users.noreply.github.com>
2026-06-11 00:13:27 +0700

    Do nothing

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: '..env');
 
   String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
   String supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
