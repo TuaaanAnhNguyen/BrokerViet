82d0c08e8511859db50a561174f14d49ae82fdc6
TuaaanAnhNguyen <ntuananh2503@gmail.com>
2026-06-15 14:03:54 +0700

    Rework the filter with categories

---
+++ frontend_flutter/lib/models/service_category_model.dart
@@ -0,0 +1,11 @@
+// lib/models/service_category_model.dart
+
+class ServiceCategoryModel {
+  final String serviceCatId;
+  final String name;
+
+  ServiceCategoryModel({
+    required this.serviceCatId,
+    required this.name,
+  });
+}
